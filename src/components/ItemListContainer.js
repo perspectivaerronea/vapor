38b47bdf47c3be3cb0efd24f9c9ee53b14874d81
Nico Padin <nicolaspadin87@gmail.com>
2022-04-19 23:19:14 -0300

    Vapor-Padin

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -3,8 +3,7 @@ import React from 'react';
 const ItemListContainer = (props) => {
   return (
     <>
-        <h1>¡Hola {props.name}!</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra nunc sit amet risus sodales, facilisis porta dolor finibus. Pellentesque convallis id orci ac mattis. Fusce fermentum dui in neque vestibulum, quis mattis orci auctor. Morbi semper dolor sapien, quis iaculis libero tempus a. Quisque at sapien dolor. Curabitur vitae laoreet tortor. Morbi et est placerat, finibus turpis et, sollicitudin risus. Aenean fermentum rhoncus est vel ultricies. Aenean vel augue lobortis augue dignissim bibendum id quis massa. Fusce fringilla tempus viverra.</p>
+        <h1>¡Hola {props.name}!</h1>        
     </>
   )
 }
