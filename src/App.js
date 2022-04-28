bbe947ca96af2726da6627f3c00cd3a12fc37e3a
Nico Padin <nicolaspadin87@gmail.com>
2022-04-28 09:16:11 -0300

    Entrega Simular Espera API - Uso Map

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App bg-yellow-50">
+    <div className="App ">
       <header className="App-header">
        <NavBar/>
       </header>      
