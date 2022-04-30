05afb21bea2215081c88e29043e8ac454342ffc5
Nico Padin <nicolaspadin87@gmail.com>
2022-04-29 23:48:09 -0300

    Correccion - Agregado ItemList entre ItemListContainer y CardJuego. Orden en los archivos

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,9 +3,9 @@ import CardJuego from './CardJuego';
 
 const ItemList = ({ datoJuego }) => {
     return (
-        <>
+        <div className="bg-white w-72 h-100 shadow-md rounded m-3">
             <CardJuego key={datoJuego.id} datoJuego={datoJuego} />
-        </>
+        </div>
     )
 }
 
