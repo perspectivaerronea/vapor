5feac99527c0f639cbf9a31591c8c551c4149f9a
Nico Padin <nicolaspadin87@gmail.com>
2022-05-13 20:00:33 -0300

    Desafio: CartContext & Sincronizar Counter - Aplicación Mejoras Sugeridas v2

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -3,37 +3,88 @@ import { Link } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
 import irAlCarrito from "../imagenes/online-shopping.png";
 
-const ItemCount = (props) => {
+const ItemCount = ({ detalle }) => {
 
-    const {enStock} = useContext(CartContext);
+    //Defino el consumer del contexto
+    const { agregarAlCarrito, eliminarDelCarrito, enStock, hayStock } = useContext(CartContext);
 
-    const [cantidadDisponible, setCantidadDisponible] = useState(props.stock);
+
+    const [cantidadDisponible, setCantidadDisponible] = useState(detalle.stock);
     const [disponible, setDisponible] = useState(true);
 
-    function calcularExistenciasDisponibles(){
-        var cantidadTotal = props.stock-props.cantidad;
+    const [cantidad, setCantidad] = useState(detalle.cantidad);
+    const [stock, setStock] = useState(detalle.stock);
+    const [enElCarrito, setEnElCarrito] = useState(false);
+
+    function handlerAgregarAlCarrito() {
+        setCantidad(cantidad + 1);
+        agregarAlCarrito({ detalle });
+        setEnElCarrito(true)
+    }
+
+    function handlerEliminarDelCarrito() {
+        setCantidad(cantidad - 1);
+        if (cantidad < 0) {
+            setCantidad(0);
+        }
+        eliminarDelCarrito({ detalle });
+        if (cantidad == 0) {
+            setEnElCarrito(false);
+        }
+    }
+
+    function estadoCarrito() {
+        return (cantidad > 0 ? true : false);
+    }
+
+    function quedaStock() {
+        return ((stock - cantidad == 0) ? false : true);
+    }
+
+
+    function calcularExistenciasDisponibles() {
+        var cantidadTotal = detalle.stock - detalle.cantidad;
         setCantidadDisponible(cantidadTotal);
-        if(cantidadTotal <= 0){
-            setDisponible(false);    
+        if (cantidadTotal <= 0) {
+            setDisponible(false);
         } else {
             setDisponible(true);
         }
-        enStock(props.stock,props.cantidad);
+        enStock(detalle.stock, detalle.cantidad);
     }
 
     useEffect(() => {
+        estadoCarrito();
+        quedaStock();
+        enStock(detalle.stock, detalle.cantidad);
         calcularExistenciasDisponibles();
-    })
+    }, [enElCarrito, cantidad])
 
 
     return (
-        <div className="flex items-center">  
-            {disponible ?
-                <div className="mx-auto text-sm text-blue-600 mt-5 font-semibold">(Stock: {cantidadDisponible})</div>            
-                :
-                <Link to="/cart"  className="mx-auto"><button className="mx-auto flex items-center"><img src={irAlCarrito} className="w-20"/><div className="text-sm bg-gray-500 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full ">Finalizar Compra</div></button></Link>
-            }
-        </div>
+        <>
+            <div className="flex justify-center items-center flex-wrap m-3">
+                {quedaStock() ?
+                    <button onClick={handlerAgregarAlCarrito} className="bg-gray-500 hover:bg-green-500 text-white font-bold py-2 px-4 rounded-full" > Agregar al Carrito</button>
+                    :
+                    <button className="bg-gray-500 text-white font-bold py-2 px-4 rounded-full">Agregar al Carrito</button>
+                }
+                <div className="text-lg font-bold ml-2 mr-2">{detalle.cantidad}</div>
+                {estadoCarrito() ?
+                    <button onClick={handlerEliminarDelCarrito} className="bg-gray-500 hover:bg-red-500 text-white font-bold py-2 px-4 rounded-full" >Eliminar del Carrito</button>
+                    :
+                    <button className="bg-gray-500 text-white font-bold py-2 px-4 rounded-full">Eliminar del Carrito</button>
+                }
+            </div>
+            <div className="mx-auto text-sm text-blue-600 mt-5 font-semibold">(Stock: {cantidadDisponible})</div>
+            <div className="flex items-center">                
+                {cantidad != 0 ?
+                    <Link to="/cart" className="mx-auto"><button className="mx-auto flex items-center"><img src={irAlCarrito} className="w-20" /><div className="text-sm bg-gray-500 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full ">Finalizar Compra</div></button></Link>
+                    :
+                    <></>
+                }
+            </div>
+        </>
     )
 }
 export default ItemCount
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -3,46 +3,6 @@ import { CartContext } from "../context/CartContext";
 import ItemCount from "./ItemCount";
 
 const ItemDetail = ({ detalle }) => {
-
-    const [cantidad, setCantidad] = useState(detalle.cantidad);
-    const [stock, setStock] = useState(detalle.stock);
-    const [enElCarrito, setEnElCarrito] = useState(false);
-
-
-    //Defino el consumer del contexto
-    const { agregarAlCarrito, eliminarDelCarrito, enStock, hayStock } = useContext(CartContext);
-
-    function handlerAgregarAlCarrito() {
-        setCantidad(cantidad + 1);
-        agregarAlCarrito({ detalle });
-        setEnElCarrito(true)
-    }
-
-    function handlerEliminarDelCarrito() {
-        setCantidad(cantidad - 1);
-        if (cantidad < 0) {
-            setCantidad(0);
-        }
-        eliminarDelCarrito({ detalle });
-        if (cantidad == 0) {
-            setEnElCarrito(false);
-        }
-    }
-
-    function estadoCarrito() {
-        return (cantidad > 0 ? true : false);
-    }
-
-    function quedaStock() {
-        return ((stock - cantidad == 0) ? false : true);
-    }
-
-    useEffect(() => {
-        estadoCarrito();
-        quedaStock();
-        enStock(detalle.stock, detalle.cantidad);
-    }, [enElCarrito, cantidad])
-
     return (
         <>
             <div className="bg-white rounded mx-auto p-5 w-1/2">
@@ -54,20 +14,7 @@ const ItemDetail = ({ detalle }) => {
                     <p>{detalle.descripcion}</p>
                     <h2 className="text-red-600 mt-5">{detalle.precio} {detalle.moneda}</h2>
                 </div>
-                <div className="flex justify-center items-center flex-wrap m-3">
-                    {quedaStock() ?
-                        <button onClick={handlerAgregarAlCarrito} className="bg-gray-500 hover:bg-green-500 text-white font-bold py-2 px-4 rounded-full" > Agregar al Carrito</button>
-                        :
-                        <button className="bg-gray-500 text-white font-bold py-2 px-4 rounded-full">Agregar al Carrito</button>
-                    }
-                    <div className="text-lg font-bold ml-2 mr-2">{detalle.cantidad}</div>
-                    {estadoCarrito() ?
-                        <button onClick={handlerEliminarDelCarrito} className="bg-gray-500 hover:bg-red-500 text-white font-bold py-2 px-4 rounded-full" >Eliminar del Carrito</button>
-                        :
-                        <button className="bg-gray-500 text-white font-bold py-2 px-4 rounded-full">Eliminar del Carrito</button>
-                    }
-                </div>                
-                <ItemCount stock={detalle.stock} cantidad={detalle.cantidad} />
+                <ItemCount detalle={detalle}/>
             </div>
         </>
     )
