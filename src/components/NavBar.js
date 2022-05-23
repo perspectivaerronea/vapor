db93b6751f153abc0f1b9653d5da8b4342630a0d
Nico Padin <nicolaspadin87@gmail.com>
2022-05-23 00:28:13 -0300

    Desafio: Update Mejora Código

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Carwidget from './Carwidget';
 import logo from '../imagenes/logosteam.png';
 import { Link } from 'react-router-dom';
+import { CartContext } from '../context/CartContext';
 
 const NavBar = () => {
+
+    const { finCompra, limpiarCarrito } = useContext(CartContext);   
+
     return (
         <div className="container flex items-center align-middle  px-5 pt-1">
-            <Link to="/" className="flex flex-none items-center align-middle px-2 font-['Fira Sans'] font-bold text-black w-1/12">
+            <Link to="/" onClick={finCompra && limpiarCarrito} className="flex flex-none items-center align-middle px-2 font-['Fira Sans'] font-bold text-black w-1/12">
                 <img src={logo} alt='logo' className="m-2 h-10 w-10"/> 
                 <span>Vapor</span>
             </Link>
             <nav className="flex-1 justify-between m-4 w-9/12">
-                <Link to="/steam" className="hover:text-red-300 m-2"> Steam</Link>
-                <Link to="/epic" className="hover:text-red-300 m-2"> Epic</Link>
-                <Link to="/itchio" className="hover:text-red-300 m-2"> Itchio</Link>
+                <Link to="/steam" onClick={finCompra && limpiarCarrito} className="hover:text-red-300 m-2"> Steam</Link>
+                <Link to="/epic" onClick={finCompra && limpiarCarrito} className="hover:text-red-300 m-2"> Epic</Link>
+                <Link to="/itchio" onClick={finCompra && limpiarCarrito} className="hover:text-red-300 m-2"> Itchio</Link>
             </nav>
             <Carwidget className="w-1/12"/>
         </div>
